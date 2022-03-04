b5709a16f05232b58fbd49fcbca4c6c702a5544f
banditsbeware <david.rademacher@mavs.uta.edu>
2022-03-04 15:48:48 -0600

    integral & basic parsing

--- ext/calculus.py
+++ ext/calculus.py
@@ -1,12 +1,28 @@
 
 from discord.ext import commands
 import os
-# INCLUDE THE WOLFRAM LIBRARY
+import json
 
-# owner commands
+from wolframclient.evaluation import WolframLanguageSession
+from wolframclient.language import wl, wlexpr
+
+with open('./config.json', 'r') as f:
+  config = json.load(f)
+
+
+import regex as re
+def wolf_to_tex(expr):
+  expr = re.sub('Global`', '', expr)
+  return expr
+
+
 class Calculus(commands.Cog, name='Calculus'):
   def __init__(self, bot):
     self.bot = bot
+    self.session = WolframLanguageSession(config["wolfram_path"])
+
+  def eval(self, expr):
+    return str( self.session.evaluate( wlexpr( expr ) ) )
     
   # Formats user input for wolfram derivate function
   # assuming command format "derivative cos(x)"
@@ -16,9 +32,15 @@ class Calculus(commands.Cog, name='Calculus'):
 
   # Formats user input for wolfram integral function
   # assuming command format "integral 3x"
-  @commands.command(name="integral")
-  async def integral(self, ctx, args):
-    await ctx.send(f'Integrate[{args}, x]')
+  @commands.command(name="integral", aliases=["i", "integrate"])
+  async def integral(self, ctx, *, args):
+    if args.find(',') == -1:
+      await ctx.send('Usage: `>integral f(x), x`')
+    else:
+      [func, var] = args.split(',')
+
+    result = self.eval( f'Integrate[{ func }, { var }]' )
+    await ctx.send( wolf_to_tex( result ) )
 
   # Formats user input for wolfram limit function
   # assuming command format "limit x=5 as x approaches 75"
@@ -29,4 +51,3 @@ class Calculus(commands.Cog, name='Calculus'):
 
 def setup(bot):
     bot.add_cog(Calculus(bot))
-
